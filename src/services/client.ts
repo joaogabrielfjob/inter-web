f637939c44751e180b616e4aa36900e77d829b67
João Job <joao.gabrielfjob@gmail.com>
2025-03-26 15:24:37 -0300

    feat: add new inter-api url

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const client = axios.create({
-  baseURL: 'http://localhost:7777',
+  baseURL: 'https://inter.api.joaojob.dev',
   headers: { 'Content-Type': 'application/json' }
 });
